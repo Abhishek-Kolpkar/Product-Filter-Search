a0c570bfd212351bfd8786b37d26552cf5598954
Abhishek <abhishekkolapkar10@gmail.com>
2022-09-01 17:51:12 +0530

    Product Filter & Search

--- script.js
+++ script.js
@@ -84,3 +84,65 @@ for (let i of products.data) {
   card.appendChild(container);
   document.getElementById("products").appendChild(card);
 }
+
+// parameter passes from button(parameter same as category)
+function filterProduct(value) {
+  // button class code
+  let buttons = document.querySelectorAll(".button-value");
+  buttons.forEach((button) => {
+    // check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  // select all cards
+  let elements = document.querySelectorAll(".card");
+  // loop through all cards
+  elements.forEach((ele) => {
+    // display all cards on 'all' button click
+    if (value == "all") {
+      ele.classList.remove("hide");
+    } else {
+      // check if element contains category class
+      if (ele.classList.contains(value)) {
+        // display element based on category
+        ele.classList.remove("hide");
+      } else {
+        // hide other element
+        ele.classList.add("hide");
+      }
+    }
+  });
+}
+
+// search button click
+document.getElementById("search").addEventListener("click", () => {
+  // initializations
+  let searchInput = document.getElementById("search-input").value;
+  let elements = document.querySelectorAll(".product-name");
+  let cards = document.querySelectorAll(".card");
+
+  // loop through all elements
+  elements.forEach((ele, index) => {
+    // check if text includes the search value
+    if (ele.innerText.includes(searchInput.toUpperCase())) {
+      // display matching card
+      cards[index].classList.remove("hide");
+    } else {
+      // hide other cards
+      cards[index].classList.add("hide");
+    }
+
+    console.log(ele.innerText.includes(searchInput.toUpperCase()));
+  });
+
+  console.log(searchInput);
+});
+
+// Initially display all products
+window.onload = () => {
+  filterProduct("all");
+};
